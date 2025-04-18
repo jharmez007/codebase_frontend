4dd289205f7cdff648d5fdb0ca685996b6b48e0b
jharmez007 <jamesagu2001@gmail.com>
2025-04-18 03:31:22 +0100

    socials updte

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -6,12 +6,12 @@ const SocialMedia = () => {
   return (
     <div className = "app__social">
       <div>
-        <a href="https://x.com/CodeBase_x" target="_blank" rel="noreferrer">
+        <a href="https://x.com/CodeBaze_x" target="_blank" rel="noreferrer">
           <FaXTwitter />
         </a>
       </div>
       <div>
-        <a href="https://youtube.com/@codebase-tv?si=DZWK0Q3iBHQZAevE" target="_blank" rel="noreferrer">
+        <a href="https://youtube.com/@codebaze-tv?si=DZWK0Q3iBHQZAevE" target="_blank" rel="noreferrer">
           <FaYoutube />
         </a>
       </div>
